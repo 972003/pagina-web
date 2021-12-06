810890c4f2f89a0b8f248ab35955ea3d03b72028
972003 <95602867+972003@users.noreply.github.com>
2021-12-06 00:09:02 -0600

    Add files via upload

---
+++ js/JS scripts.js
@@ -0,0 +1,9 @@
+alert("Bienvenido a mi pagina de ejercicios");
+function vision() {
+   /* alert("Haz dado un click");*/
+   var visionh1 = document.getElementById('visionh1');
+   visionh1.style.display(display);
+   var vision= document.getElementById('vision');
+   vision.innerHTML ='Satisfacer con excelencia a los consumidores de bebidas.';
+  
+}
